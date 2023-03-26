84c112d3685a9ed13d5bf32c101a6ad11a1a92ae
ryuichi.kasadate <divinatech.tokyo@gmail.com>
2023-03-26 19:24:56 +0900

    var関連ファイルの一括追加

---
+++ va4.php
@@ -0,0 +1,7 @@
+<?php
+$name = 'Andy';
+$Name = 'Betty';
+$NAME = 'Callol';
+echo $name;
+echo $Name;
+echo $NAME;
---
+++ var13.php
@@ -0,0 +1,6 @@
+<?php
+$price = 1000;
+$count = 10;
+
+$totalPraice = $price * $count;
+echo $totalPraice;
